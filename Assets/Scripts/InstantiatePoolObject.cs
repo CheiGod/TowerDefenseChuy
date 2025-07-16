87eb998b20d9a06279fbb1007393a5acdb0f7e3a
Jesús Enrique Barajas <jesus_barajas@outlook.com>
2025-07-15 20:36:18 -0600

    Clase2

--- Assets/Scripts/InstantiatePoolObject.cs
+++ Assets/Scripts/InstantiatePoolObject.cs
@@ -1,16 +1,50 @@
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor.Search;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class InstantiatePoolObject : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private GameObject _prefab;
+    [SerializeField]
+    private Transform _parent;
+    private List<GameObject> _pool = new List<GameObject>();
+    public void InstantiateObject(Transform target)
     {
-        
+        GameObject obj = GetPooledObject();
+        if (obj != null)
+        {
+            obj.transform.position = target.position;
+            obj.transform.rotation = target.rotation;
+            obj.SetActive(true);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public void PositionObject(GameObject obj, Vector3 position, quaternion rotation)
     {
-        
+        if (_parent != null)
+        {
+            obj.transform.SetParent(_parent, false);
+            obj.transform.SetLocalPositionAndRotation(position, rotation);
+        }
+        else
+        {
+            obj.transform.SetPositionAndRotation(position, rotation);
+        }
+        obj.SetActive(true);
+    }
+    private GameObject GetPooledObject()
+    {
+        GameObject obj = null;
+        obj = _pool.Find(x => !x.activeInHierarchy);
+        if (obj == null)
+        {
+
+        }
+        else
+        {
+            
+        }
     }
 }
